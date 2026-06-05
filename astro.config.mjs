70ade5e99be7eb8e28507bc74ecb83c2307cc07d
victorbetoni <victorbetoni@protonmail.com>
2026-06-05 11:02:28 -0300

    chore: instala e configura o tailwind para estilização

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 // @ts-check
+import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://victorbetoni.github.io',
   base: '/ds881-curriculo-GRR20244740', 
   vite: {
+    plugins: [tailwindcss()],
     server: {
       watch: {
         usePolling: true,
